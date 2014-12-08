d38bb4ccecbbf79b67b77bb8982eecd32b894e74
Kevin Conaway <kevin.conaway@jivesoftware.com>
2014-12-07 22:06:42 -0500

    Get filename from argv

--- python/main.py
+++ python/main.py
@@ -3,21 +3,24 @@ from model import plan
 import time
 import sys
 
-print "Reading in segments...",
-sys.stdout.flush()
+if len(sys.argv) != 2:
+    print 'Please provide an input file name'
+else:
+    print "Reading in segments...",
+    sys.stdout.flush()
 
-source = segment.SegmentSource(input='data/flight_paths.txt')
-print 'done!'
+    source = segment.SegmentSource(input=sys.argv[1])
+    print 'done!'
 
-print 'Calculating flight plan, this may take a minute or so...',
-sys.stdout.flush()
+    print 'Calculating flight plan, this may take a minute or so...',
+    sys.stdout.flush()
 
-start= time.time()
-flightPlanCalculator = plan.FlightPlanCalculator()
-flightPlan = flightPlanCalculator.calculate(source.segments, 0, source.lastMile)
-elapsed = int(time.time()) - int(start)
+    start= time.time()
+    flightPlanCalculator = plan.FlightPlanCalculator()
+    flightPlan = flightPlanCalculator.calculate(source.segments, 0, source.lastMile)
+    elapsed = int(time.time()) - int(start)
 
-print 'done in {0}s\n'.format(elapsed)
+    print 'done in {0}s\n'.format(elapsed)
 
-print "JetStream Segments: ", [(s.start, s.end) for s in flightPlan.jetStreamSegments]
-print "Total flight cost: ", flightPlan.totalEnergy
+    print "JetStream Segments: ", [(s.start, s.end) for s in flightPlan.jetStreamSegments]
+    print "Total flight cost: ", flightPlan.totalEnergy
